a18746779ca0a57ad5b7a42a81821be479c60e4a
linjiejiao <hpckljj@gmail.com>
2014-05-14 20:43:16 +0800

    20140514  2043

--- src/cn/ljj/musicplayer/ui/PlayingFragment.java
+++ src/cn/ljj/musicplayer/ui/PlayingFragment.java
@@ -21,8 +21,9 @@ public class PlayingFragment extends Fragment implements OnClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		mRootView = inflater.inflate(R.layout.fragment_playing, container, false);
-		lrc_view = (LrcView)mRootView.findViewById(R.id.lrc_view);
+		mRootView = inflater.inflate(R.layout.fragment_playing, container,
+				false);
+		lrc_view = (LrcView) mRootView.findViewById(R.id.lrc_view);
 		lrc_view.init();
 		mSongPic = (ImageView) mRootView.findViewById(R.id.song_pic);
 		mSongPic.setOnClickListener(this);
@@ -32,20 +33,20 @@ public class PlayingFragment extends Fragment implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
-			case R.id.song_pic:
-				if(lrc_view.getVisibility() == View.GONE){
-					setLrcs(null);
-					lrc_view.setVisibility(View.VISIBLE);
-				}else{
-					lrc_view.setVisibility(View.GONE);
-				}
-				break;
+		switch (v.getId()) {
+		case R.id.song_pic:
+			if (lrc_view.getVisibility() == View.GONE) {
+				setLrcs(null);
+				lrc_view.setVisibility(View.VISIBLE);
+			} else {
+				lrc_view.setVisibility(View.GONE);
+			}
+			break;
 		}
-		Log.e("onClick", "v="+v.getId());
+		Log.e("onClick", "v=" + v.getId());
 	}
 
-	private void setLrcs(List<String> lrcs){
+	private void setLrcs(List<String> lrcs) {
 		lrcs = new ArrayList<String>();
 		lrcs.add("srdoafhlafalksf");
 		lrcs.add("sgtdsgsdg");
@@ -56,7 +57,8 @@ public class PlayingFragment extends Fragment implements OnClickListener {
 		lrcs.add("sgtd124333333343333333333334444444444sgsdg");
 		lrcs.add("lhjjgff333333333333333333333333333333333dyadssxb");
 		lrcs.add("12tgoafsaghlafalksf");
-		lrcs.add("u2oiwyfhcnalksf");lrcs.add("srdoafhlafalksf");
+		lrcs.add("u2oiwyfhcnalksf");
+		lrcs.add("srdoafhlafalksf");
 		lrcs.add("sgtdsgsdg");
 		lrcs.add("lhjjgffdyadssxb");
 		lrcs.add("g斯蒂芬斯蒂芬速度dhoaidha  收到哦啊的啊苏打水哦啊是很f");
@@ -65,7 +67,8 @@ public class PlayingFragment extends Fragment implements OnClickListener {
 		lrcs.add("sgtdsgsdg");
 		lrcs.add("lhjjgffdyadssxb");
 		lrcs.add("12tgoafsaghlafalksf");
-		lrcs.add("u2oiwyfhcnalksf");lrcs.add("srdoafhlafalksf");
+		lrcs.add("u2oiwyfhcnalksf");
+		lrcs.add("srdoafhlafalksf");
 		lrcs.add("sgtdsgsdg");
 		lrcs.add("lhjjgffdyadssxb");
 		lrcs.add("润肤打开alks阿什佛ifhocadsadhoaidha  收到哦啊的啊苏打水哦啊是很f");
